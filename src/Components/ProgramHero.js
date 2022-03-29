e1156736f3d8b3dcffd8997358ed6856f172e69b
Sheila Strahan <smstrahan@gmail.com>
2022-03-28 18:36:30 -0600

    program hero section

---
+++ src/Components/ProgramHero.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Row, Col } from 'react-bootstrap';
+
+export default function ProgramHero() {
+    return (
+        <Container className="programhero-container" fluid>
+        <Row className="m-3">
+            <h1 className="programhero-title">Our Village Comes to Life</h1>
+       </Row>
+      </Container>
+    )
+}
--- src/Pages/ProgrammingPage.js
+++ src/Pages/ProgrammingPage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import HomeOurPrograms from '../Components/HomeOurPrograms';
+import ProgramHero from '../Components/ProgramHero';
 
 
 
 export default function ProgrammingPage() {
     return (
         <>
-            
+            <ProgramHero />
             <HomeOurPrograms />
             
             
