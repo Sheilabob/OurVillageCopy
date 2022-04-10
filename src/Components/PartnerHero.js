bedfea06195f3a7dcc96c88ddcf816424aa90178
Sheila Strahan <smstrahan@gmail.com>
2022-04-09 20:36:53 -0600

    partners hero section

---
+++ src/Components/PartnerHero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container, Row, Col } from 'react-bootstrap';
+
+export default function PartnerHero() {
+    return (
+        <Container className="partnerhero-container" fluid>
+        <Row className="m-5">
+             
+       </Row>
+
+      </Container>
+    )
+}
--- src/Pages/PartnersPage.js
+++ src/Pages/PartnersPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PartnerHero from '../Components/PartnerHero';
 
 
 
 export default function ProgrammingPage() {
     return (
         <>
+            <PartnerHero />
             
             
 
